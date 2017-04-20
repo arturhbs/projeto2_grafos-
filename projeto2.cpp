ae3817a9b3906b3de73d59bc5badab22b25091bc
arturhbs <arturhenrique71@gmail.com>
2017-04-20 18:48:07 -0300

    Update projeto2.cpp

--- projeto2.cpp
+++ projeto2.cpp
@@ -1,9 +1,37 @@
+/*Universidade de Brasilia
+Instituto de Ciencias Exatas
+Departamento de Ciencia da Computacao
+
+Teroria e aplicação de grafos - 01/2017
+
+Alunos(a): Artur Henrique Brandao de Souza	- 15/0118783 
+		   Marcos Paulo Batalha Bispo - 15O154208
+							
+Versão do compilador: gcc(GCC) 4.8.1
+
+Descricao:	Projeto2 = 
+*/
+
+
 #include <iostream>
+#include <string>
+#include <list>
 
 using namespace std;
+
+typedef  struct materias{
+	int codigo;
+	string nome;
+	int creditos;
+	int dificuldade;
+	list<materias> t_materias;
+
+
+}t_materia;
+
 
 int main(){
 	
 
-	return 0;
+	return 0; 	
 }
