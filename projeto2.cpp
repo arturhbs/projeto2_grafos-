78a65dd8d6782e86a9193bd11079417e4220c60c
arturhbs <arturhenrique71@gmail.com>
2017-04-30 15:39:38 -0300

    Update projeto2.cpp

--- projeto2.cpp
+++ projeto2.cpp
@@ -66,13 +66,21 @@ void PegarGrauZero(){
 		if(materia[i].quantidade_materias_loop == 0){ /*gravar os vertices que contem grau zero na lista 'grauzero' */
 			codigo_materia = materia[i].codigo;
 			grauzero.push_back(codigo_materia);
+			
+					 /*	for(int k=0;k<35; k++){	 TESTE MEU PARA VER OQ ESTÁ PRINTANDO
+					 		if(materia[k].codigo == codigo_materia){
+					 			cout << "Materia = "<<materia[k].nome << endl;
+					 			break;
+					 		}
+					 	}
+					 	getchar();*/
 			for(j=0;j<35; j ++){
 				if(materia[j].quantidade_materias_loop != 0){ /*verificar apenas as materias que o grau é diferente de zero*/
 					for (list<int>::iterator it1=materia[j].lista_materias.begin(); it1 != materia[j].lista_materias.end(); ++it1){/* pesquisar na lista de cada um se há a materia como pre-requisito*/
 					 	if(*it1 == codigo_materia){
 					 		materia[j].quantidade_materias_loop = materia[j].quantidade_materias_loop -1;/*diminui o contador da materia*/	
-					 		
-					 	}
+					 		cout << "materia = " << materia[j].nome<< endl;
+					 	}	
 					}
 				}	
 			}
@@ -81,12 +89,6 @@ void PegarGrauZero(){
 	}
 	for (list<int>::iterator it=grauzero.begin(); it != grauzero.end(); ++it){/* printar lista de cada um*/
 	 	cout << ' ' << *it << ' ';
-	 	for(i=0;i<35; i++){
-	 		if(materia[i].codigo == *it){
-	 			cout << "Materia = "<<materia[i].nome << " i == "<< i<< endl;
-	 			break;
-	 		}
-	 	}
 		getchar();
 	}
 
