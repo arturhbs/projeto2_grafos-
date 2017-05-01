d9f4b359973d72f4e5b4da61d9d5fdbefdbaf80e
arturhbs <arturhenrique71@gmail.com>
2017-05-01 16:42:31 -0300

    Update projeto2.cpp

--- projeto2.cpp
+++ projeto2.cpp
@@ -24,23 +24,25 @@ using namespace std;
 typedef  struct materias{
 	int codigo;
 	string nome;
-	int creditos;
-	int dificuldade;
+	float creditos;
+	float dificuldade;
 	int quantidade_materias_loop;
 	list<int> lista_materias;
-	list<int> lista_valorAresta;
+	list<float> lista_valorAresta;
 	
 
 }t_materia;
-
-t_materia materia[50];
-
+/*variaveis globais --------------------------------------------------------------*/
+t_materia materia[46];
+t_materia materia_copia[46];
+list<int> grauzero;	/*lista para gravar a ordem topologica utilizando Khan*/
+/* ------------------------------------------------------------------------------*/
 void CriarGrafo(){
 	string	line;
 	ifstream fp("materias.txt");
 	int i=0, j;
 	int valor;
-	int valorAresta;
+	float valorAresta;
 	while(!fp.eof()){
 		getline(fp, line);/*pega uma linha inteira do .txt*/
 		stringstream is(line);/* 'is' passa a ser um tipo de leitura para a string no arquivo*/
@@ -56,11 +58,11 @@ void CriarGrafo(){
 		
 		i++;
 	}
-	for(i=0;i<35;i++){
+	for(i=0;i<46;i++){
 		if(materia[i].quantidade_materias_loop != 0){ /*verificar apenas as materias que o grau é diferente de zero*/
 			for (list<int>::iterator it=materia[i].lista_materias.begin(); it != materia[i].lista_materias.end(); ++it){/* pesquisar na lista de cada um se há a materia como pre-requisito*/
 				valor =	*it;
-				for(int k=0; k<35;k++){	/*fazer um loop nas materias já cadastradas para verificar com as da lista */
+				for(int k=0; k<46;k++){	/*fazer um loop nas materias já cadastradas para verificar com as da lista */
 					if(materia[k].codigo == valor){
 						valorAresta = materia[k].creditos * materia[k].dificuldade;	/* Valor da aresta = Creditos * dificuldade*/
 						materia[i].lista_valorAresta.push_back(valorAresta);/*colocar o valor final na lista*/
@@ -73,12 +75,11 @@ void CriarGrafo(){
 
 }
 
-	list<int> grauzero;	/*lista para gravar a ordem topologica utilizando Khan*/
 void PegarGrauZero(){
 	int codigo_materia, i=0, j;
 	bool ja_existe;
 
-	while(i !=35 ){
+	while(i !=46 ){
 		ja_existe = false;
 		if(materia[i].quantidade_materias_loop == 0){ /*gravar os vertices que contem grau zero na lista 'grauzero' */
 			for(list<int>::iterator it= grauzero.begin(); it!= grauzero.end(); it++){/*percorre a lista grauzero */
@@ -93,7 +94,7 @@ void PegarGrauZero(){
 				grauzero.push_back(codigo_materia);/*coloca o codigo da materia no final da lista*/ 
 				i=-1;/*ao ele colocar um novo elemento na lista grauzero , ele voltara nas materias e verificara se tem alguma materia que ficou para trás, caso o arquivo texto ja nao esteja em uma ordem topológica previamente*/
 
-				for(j=0;j<35; j ++){
+				for(j=0;j<46; j ++){
 					if(materia[j].quantidade_materias_loop != 0){ /*verificar apenas as materias que o grau é diferente de zero*/
 						for (list<int>::iterator it1=materia[j].lista_materias.begin(); it1 != materia[j].lista_materias.end(); ++it1){/* pesquisar na lista de cada um se há a materia como pre-requisito*/
 						 	if(*it1 == codigo_materia){
@@ -111,12 +112,12 @@ void PegarGrauZero(){
 void PrintarGrafo(){
 
 	int i;
-	for(i=0;i<35;i++){
+	for(i=0;i<46;i++){
 		cout <<"Codigo  = " <<materia[i].codigo ;
 		if(!materia[i].lista_materias.empty()){		/* verifica se a lista de materias está vazia, ja que se estiver a de valor de aresta tambem estara*/
 			cout << " Pre-requisito(s) = ";		
 			list<int>::iterator it;
-			list<int>::iterator it_aux;
+			list<float>::iterator it_aux;
 			it = materia[i].lista_materias.begin();	/*pegar o primeiro elemento da lista de materias*/
 			it_aux = materia[i].lista_valorAresta.begin(); /*pegar o primeiro elemento da lista dos valores das arestas*/
 			while(it != materia[i].lista_materias.end()){
@@ -133,22 +134,37 @@ void PrintarOrdemTopologica(){
 	int j=1;	
 	for(list<int>::iterator it= grauzero.begin(); it!= grauzero.end(); it++){
 		cout << *it ;
-		for(int i=0;i<35;i++){
+		for(int i=0;i<46;i++){
 			if(*it == materia[i].codigo){
 				cout << " == " << materia[i].nome<<endl; ;
 				break;
 			}
 		}
-		j++;
 	}
-	cout << " j == " << j << endl;	
 }
 
 
 int main(){
-	int escolha;
+	int escolha, aux;
 
 	CriarGrafo();
+	for(int i=0;i<46;i++){ /*fazer uma copia do grafo atual para poder modificar salvando o grafo original*/
+		materia_copia[i].codigo = materia[i].codigo;
+		materia_copia[i].nome = materia[i].nome;
+		materia_copia[i].creditos = materia[i].creditos;
+		materia_copia[i].dificuldade = materia[i].dificuldade;
+		materia_copia[i].quantidade_materias_loop = materia[i].quantidade_materias_loop;
+		for(list<int>::iterator it = materia[i].lista_materias.begin(); it!= materia[i].lista_materias.end();it++){
+			aux = *it;
+			materia_copia[i].lista_valorAresta.push_back(aux);
+			
+		}
+		for(list<float>::iterator it1 = materia[i].lista_valorAresta.begin(); it1!= materia[i].lista_valorAresta.end();it1++){
+			aux = *it1;
+			materia_copia[i].lista_valorAresta.push_back(aux);
+		}
+
+	}
 	PegarGrauZero();
 
 	cout << "Escolha a opcao que deseja : \n 1- Printar grafo \n 2- Printar grafo em ordem topológica\n ";
